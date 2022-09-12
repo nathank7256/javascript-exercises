4891e8befd863385c83d2a2b954f107ee16b9467
nathank7256 <nathank7256@gmail.com>
2022-09-12 05:14:09 -0500

    Finished findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(a) {
+    let currentTime = new Date();
+    let ages = a.sort((a, b) => ((b.yearOfDeath ?? currentTime.getFullYear()) - b.yearOfBirth) - ((a.yearOfDeath ?? currentTime.getFullYear()) - a.yearOfBirth));
+    console.table(ages)
+    return(ages[0]);
 };
 
 // Do not edit below this line
