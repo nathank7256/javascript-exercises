ba7ae89c2e13900be8a7b68fa27fa64396b50485
nathank7256 <nathank7256@gmail.com>
2022-09-12 03:01:50 -0500

    Finished calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return Number(a) + Number(b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return  a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let final = 0;
+	for(element in a){
+    final += Number(a[element]);
+  }
+  return final;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let final = a[0];
+	for(element in a){
+    if(element != 0){
+      final *= Number(a[element]);
+    }
+  }
+  return final;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let final = a;
+	for(let i = 1; i < b; i++){
+    final *= a
+    console.log(final);
+  }
+  return final;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let number = 0;
+  let final = 1;
+	for(let i = 0; i < a; i++){
+    number += 1;
+    final *= number;
+  }
+  return final;
 };
 
 // Do not edit below this line
