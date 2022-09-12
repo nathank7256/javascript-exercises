b7a9453768cc8e2c45b35c0f953733309a4976d1
nathank7256 <nathank7256@gmail.com>
2022-09-12 03:42:52 -0500

    Finished fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    let sequence = [1,1];
+    for(let i = 0; i < a; i++){
+        sequence.push(Number(sequence[i]) + Number(sequence[i + 1]));
+    }
+    return sequence[a - 1];
 };
+
+console.log(fibonacci(6));
 
 // Do not edit below this line
 module.exports = fibonacci;
